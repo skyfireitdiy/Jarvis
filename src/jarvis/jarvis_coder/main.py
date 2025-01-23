d1a59e4a5f2fbff54ae78cd62c57ed0a6a0f514a
skyfire <skyfireitdiy@hotmail.com>
2025-01-23 04:10:17 +0000

    修复f-string

--- src/jarvis/jarvis_coder/main.py
+++ src/jarvis/jarvis_coder/main.py
@@ -153,7 +153,7 @@ class JarvisCoder:
 """
         for i, file in enumerate(related_files):
             if len(prompt) > 30 * 1024:
-                PrettyOutput.print(f"避免上下文超限，丢弃低相关度文件：{file["file_path"]}", OutputType.WARNING)
+                PrettyOutput.print(f'避免上下文超限，丢弃低相关度文件：{file["file_path"]}', OutputType.WARNING)
                 continue
             prompt += f"""{i}. {file["file_path"]}\n"""
             prompt += f"""文件内容:\n"""
