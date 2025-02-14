4466ddb11c49dc4dbb36138b58c4867c8e04ef5f
skyfire <skyfireitdiy@hotmail.com>
2025-02-14 23:24:04 +0800

    feat(git_commiter): suppress output during commit message generation

--- src/jarvis/tools/git_commiter.py
+++ src/jarvis/tools/git_commiter.py
@@ -44,6 +44,7 @@ class GitCommitTool:
             '''
             PrettyOutput.print("Generate commit message...", OutputType.SYSTEM)
             platform = PlatformRegistry().get_codegen_platform()
+            platform.set_suppress_output(True)
             commit_message = platform.chat_until_success(prompt)
             commit_message = self._extract_commit_message(commit_message)
             PrettyOutput.print("Commit...", OutputType.INFO)
