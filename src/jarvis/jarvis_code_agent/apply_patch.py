66c05692dd7a23c92441d2875da248c2c22ad294
skyfire <skyfireitdiy@hotmail.com>
2025-02-14 23:02:39 +0800

    refactor(code_agent): remove apply_patch tool and simplify commit flow

---
+++ src/jarvis/jarvis_code_agent/apply_patch.py
@@ -0,0 +1,13 @@
+# from typing import Dict, Any, List, Tuple
+# import os
+# from jarvis.tools.read_code import ReadCodeTool
+# from jarvis.utils import OutputType, PrettyOutput
+
+
+
+
+# def execute(self,  patches: List[Dict[str, List[str]]]) -> Tuple[bool, str, str]:
+#     err = ""
+#     output = ""
+#     for patch in patches:
+        
