ef6129b070351cad69262877be92a23de0a1a154
skyfire <skyfireitdiy@hotmail.com>
2025-03-02 16:26:53 +0800

    'chore(pyproject): Remove jarvis-dev entry and align CLI entry points Remove jarvis-dev entry from pyproject.toml and update setup.py to ensure consistency in CLI entry points This change prevents potential conflicts and ensures accurate command-line tool availability'

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
             "jarvis-platform-manager=jarvis.jarvis_platform_manager.main:main",
             "jarvis-git-commit=jarvis.jarvis_tools.git_commiter:main",
             "jarvis-code-review=jarvis.jarvis_tools.code_review:main",
-            "jarvis-dev=jarvis.jarvis_dev.main:main",
+            "jgc=jarvis.jarvis_tools.git_commiter:main",
             "jgc=jarvis.jarvis_tools.git_commiter:main",
         ],
     },
