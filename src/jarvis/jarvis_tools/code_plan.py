5d312b6a0b86b190e12a5334b10e2cfba752641b
skyfire <skyfireitdiy@hotmail.com>
2025-04-05 20:02:39 +0800

    'refactor(code_plan): 将CodePlanner工具平台从普通平台切换为思考平台
    修改CodePlanTool中CodePlanner工具的platform参数，从get_normal_platform()改为get_thinking_platform()'

--- src/jarvis/jarvis_tools/code_plan.py
+++ src/jarvis/jarvis_tools/code_plan.py
@@ -105,7 +105,7 @@ class CodePlanTool:
                     name="CodePlanner",
                     description="分析代码修改需求并制定详细计划",
                     summary_prompt=summary_prompt,
-                    platform=PlatformRegistry().get_normal_platform(),
+                    platform=PlatformRegistry().get_thinking_platform(),
                     output_handler=[tool_registry],
                     execute_tool_confirm=False,
                     auto_complete=self.auto_complete
