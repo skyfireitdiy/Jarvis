9b7bc452ab3af20fd944ac8a0937d0eee768c068
skyfire <skyfireitdiy@hotmail.com>
2025-07-21 22:27:34 +0800

    Bump version to 0.1.224

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages  # type: ignore
 
 setup(
     name="jarvis-ai-assistant",
-    version="0.1.223",
+    version="0.1.224",
     author="skyfire",
     author_email="skyfireitdiy@hotmail.com",
     description="An AI assistant that uses various tools to interact with the system",
