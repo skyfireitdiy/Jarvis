4586ec165775036c789251fd8f46faa9e550b7f2
skyfire <skyfireitdiy@hotmail.com>
2026-02-26 22:01:05 +0800

    Bump version to 2.0.13

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class PostInstallCommand(install):
 
 setup(
     name="jarvis-ai-assistant",
-    version="2.0.12",
+    version="2.0.13",
     author="skyfire",
     author_email="skyfireitdiy@hotmail.com",
     description="An AI assistant that uses various tools to interact with the system",
