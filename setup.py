e5ed8f14013082862e457a0445fa0f5276546d16
skyfire <skyfireitdiy@hotmail.com>
2026-04-27 00:12:16 +0800

    Bump version to 3.0.32

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="jarvis-ai-assistant",
-    version="3.0.31",
+    version="3.0.32",
     author="skyfire",
     author_email="skyfireitdiy@hotmail.com",
     description="An AI assistant that uses various tools to interact with the system",
