a6508d69cf6fcad19a928a38754e298967007a8c
skyfire <skyfireitdiy@hotmail.com>
2026-06-02 15:01:13 +0000

    CheckPoint #578

--- src/jarvis/jarvis_agent/skill_discovery/sources/__init__.py
+++ src/jarvis/jarvis_agent/skill_discovery/sources/__init__.py
@@ -2,12 +2,8 @@
 """技能发现源模块"""
 
 from .base import ISkillSource, SkillResult
-from .skillhub import SkillHubSource
-from .github import GitHubSkillSource
 
 __all__ = [
     'ISkillSource',
     'SkillResult',
-    'SkillHubSource',
-    'GitHubSkillSource',
 ]
